0c920e908cd8db5c6f339835852dbab44dc055b2
Stevefe123 <ogbesteve123@gmail.com>
2025-07-01 02:04:26 +0000

    Fix: Add types for layout children prop

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
 // app/layout.js
 import './globals.css'
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: {
+  children: React.ReactNode // <-- THIS IS THE FIX
+}) {
   return (
     <html lang="en">
       <body className="bg-gray-900 text-white">{children}</body>
